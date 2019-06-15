f56ed90b87c2a2cbf59e23e0e7a354f75765e413
chanyou0311 <chanyou0311@gmail.com>
2019-06-15 16:47:32 +0900

    [add] templates

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "yurukai",
 ]
 
 MIDDLEWARE = [
@@ -75,9 +76,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 if DEBUG is True:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
         }
     }
 else:
--- yurukai/views.py
+++ yurukai/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, "yurukai/index.html")
+
+
+def about(request):
+    return render(request, "yurukai/about.html")
