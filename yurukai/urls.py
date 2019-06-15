d282f2853b0fb47c48106bcd4cf0c03712141a70
kazari <kazoo.g@gmail.com>
2019-06-15 19:47:15 +0900

    delete unused routes

--- yurukai/urls.py
+++ yurukai/urls.py
@@ -4,15 +4,15 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("about/", views.about, name="about"),
+   # path("about/", views.about, name="about"),
     path("join_s/", views.join_s, name="join_s"),
-    path("join_t/", views.join_t, name="join_t"),
+   # path("join_t/", views.join_t, name="join_t"),
     path("join_s_conf/", views.join_s_conf, name="join_s_conf"),
-    path("join_t_conf/", views.join_t_conf, name="join_t_conf"),
+   # path("join_t_conf/", views.join_t_conf, name="join_t_conf"),
 
     path("reg_s/", views.reg_s, name="reg_s"),
-    path("teg_t/", views.reg_t, name="reg_t"),
-    path("reg_s_conf/", views.reg_s_conf, name="reg_s_conf"),
-    path("reg_t_conf/", views.reg_t_conf, name="reg_t_conf")
+   # path("teg_t/", views.reg_t, name="reg_t"),
+    path("reg_s_conf/", views.reg_s_conf, name="reg_s_conf")
+   # path("reg_t_conf/", views.reg_t_conf, name="reg_t_conf")
 
 ]
