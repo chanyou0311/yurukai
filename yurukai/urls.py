577e0dc2528389163005dbf494596e246a042634
kazari <kazoo.g@gmail.com>
2019-06-15 17:32:27 +0900

    add routing

--- yurukai/urls.py
+++ yurukai/urls.py
@@ -4,5 +4,15 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("about/", views.about, name="about")
+    path("about/", views.about, name="about"),
+    path("join_s/", views.join_s, name="join_s"),
+    path("join_t/", views.about, name="join_t"),
+    path("join_s_conf/", views.about, name="join_s_conf"),
+    path("join_t_conf/", views.about, name="join_t_conf"),
+
+    path("reg_s/", views.about, name="reg_s"),
+    path("teg_t/", views.about, name="reg_t"),
+    path("reg_s_conf/", views.about, name="reg_s_conf"),
+    path("reg_t_conf/", views.about, name="reg_t_conf")
+
 ]
--- yurukai/views.py
+++ yurukai/views.py
@@ -4,6 +4,17 @@ from django.shortcuts import render
 def index(request):
     return render(request, "yurukai/index.html")
 
-
 def about(request):
     return render(request, "yurukai/about.html")
+
+def join_s(request):
+    return render(request, "yurukai/join_s.html")
+
+def join_t(request):
+    return render(request, "yurukai/join_t.html")
+
+def join_s_conf(request):
+    return render(request, "yurukai/join_s_conf.html")
+
+def join_t_conf(request):
+    return render(request, "yurukai/join_t_conf.html")
