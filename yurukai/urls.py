e719f61bded392e049fa98cb3ce78487cff70931
chanyou0311 <chanyou0311@gmail.com>
2019-06-15 20:08:30 +0900

    empty commit

--- yurukai/urls.py
+++ yurukai/urls.py
@@ -3,16 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
     path("about/", views.about, name="about"),
     path("join_s/", views.join_s, name="join_s"),
     path("join_t/", views.join_t, name="join_t"),
     path("join_s_conf/", views.join_s_conf, name="join_s_conf"),
     path("join_t_conf/", views.join_t_conf, name="join_t_conf"),
-
     path("reg_s/", views.reg_s, name="reg_s"),
     path("teg_t/", views.reg_t, name="reg_t"),
     path("reg_s_conf/", views.reg_s_conf, name="reg_s_conf"),
-    path("reg_t_conf/", views.reg_t_conf, name="reg_t_conf")
-
+    path("reg_t_conf/", views.reg_t_conf, name="reg_t_conf"),
 ]
--- yurukai/views.py
+++ yurukai/views.py
@@ -1,20 +1,29 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Yurukai
 
 
-def index(request):
-    return render(request, "yurukai/index.html")
+class IndexView(ListView):
+    model = Yurukai
+    template_name = "yurukai/index.html"
+
 
 def about(request):
     return render(request, "yurukai/about.html")
 
+
 def join_s(request):
     return render(request, "yurukai/join_s.html")
+
 
 def join_t(request):
     return render(request, "yurukai/join_t.html")
 
+
 def join_s_conf(request):
     return render(request, "yurukai/join_s_conf.html")
+
 
 def join_t_conf(request):
     return render(request, "yurukai/join_t_conf.html")
@@ -23,11 +32,14 @@ def join_t_conf(request):
 def reg_s(request):
     return render(request, "yurukai/reg_s.html")
 
+
 def reg_t(request):
     return render(request, "yurukai/reg_t.html")
 
+
 def reg_s_conf(request):
     return render(request, "yurukai/reg_s_conf.html")
+
 
 def reg_t_conf(request):
     return render(request, "yurukai/reg_t_conf.html")
