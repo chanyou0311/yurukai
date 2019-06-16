7f0cfce4b249232d38cc8912417f64c092714e54
chanyou0311 <chanyou0311@gmail.com>
2019-06-16 09:17:46 +0900

    [modify] routing

--- yurukai/urls.py
+++ yurukai/urls.py
@@ -6,8 +6,9 @@ from . import views
 app_name = "yurukai"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    path("<int:pk>/", views.YurukaiDetailView.as_view(), name="detail"),
-    path("create/", views.YurukaiCreateView.as_view(), name="create"),
+    path("yurukai/", views.IndexView.as_view(), name="yurukai_list"),
+    path("yurukai/<int:pk>/", views.YurukaiDetailView.as_view(), name="yurukai_detail"),
+    path("yurukai/create/", views.YurukaiCreateView.as_view(), name="yurukai_create"),
     path("users/<int:pk>/", views.UserDetailView.as_view(), name="users_detail"),
     path("reg_s/", views.reg_s, name="reg_s"),
     path("reg_s_conf/", views.reg_s_conf, name="reg_s_conf"),
